6d6a2220eb1841e32e937809adfc8bda212cd47d
nsmlenceva <nsmlenceva@rambler.ru>
2022-09-14 15:15:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [present, setPresent] = useState(data);
+  
+  const removePresent = (id) => {
+    let newPresent = present.filter(item => item.id!==id)
+    setPresent(newPresent);
+  }
+  
+   return (
+     <div>
+       <div className='container'>
+         <h1>Топ {present.length} новогодних подарков</h1>
+       </div>
+       {present.map(presentItem => {
+         const {id, name} = presentItem;
+         return(
+           <div key={id} className='container'>
+             <p>{id} - {name}</p>
+             <button onClick={() => removePresent(id)} className='btnDel' ></button>
+           </div>
+         )
+       }
+       )}
+       <div className='container'>
+         <button onClick={()=> setPresent([])} className='btnClear'>Очистить всё</button>
+       </div>
+     </div>
+   );
 }
+
 
 export default App;
